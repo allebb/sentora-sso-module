8e2a2ac5419f279f23c0fa9f627003ce8dbf8039
Bobby Allen <ballen@bobbyallen.me>
2015-02-23 19:09:32 +0000

    Added session killer functionality when an SSO attempt is made prior to auth to kill any stale sessions also removed some development PHP error trapping code.

--- src/hooks/OnBoot.hook.php
+++ src/hooks/OnBoot.hook.php
@@ -11,28 +11,29 @@ global $zdbh;
 
 if (isset($_REQUEST['ssoToken']) && isset($_REQUEST['ssoInit'])) {
 
-    ini_set('display_startup_errors', 1);
-    ini_set('display_errors', 1);
-    error_reporting(-1);
-
     require_once __DIR__ . '/../libs/SSO.php';
 
     $conf = json_decode(ctrl_options::GetSystemOption('sso_config'));
 
+    // Force logout when a valid-looking SSO request is made.
+    runtime_hook::Execute('OnLogout');
+    ctrl_auth::KillSession();
+    ctrl_auth::KillCookies();
+    
     $sso = SSO::getInstance();
     $sso->setKey($conf->key);
     $sso->setIv($_REQUEST['ssoInit']);
-
+    
     $credentials = $sso->decrypt($_REQUEST['ssoToken'])->ssoData();
 
-    if (isset($credentials['id']) && isset($credentials['user'])) {
+    if (isset($credentials['uid']) && isset($credentials['user'])) {
         $sql = $zdbh->prepare("SELECT ac_id_pk, ac_user_vc"
             . " FROM x_accounts"
             . " WHERE ac_id_pk = :uid"
             . " AND ac_user_vc = :username"
             . " AND ac_deleted_ts IS NULL"
             . " AND ac_enabled_in = 1");
-        $sql->bindParam(':uid', $credentials['id']);
+        $sql->bindParam(':uid', $credentials['uid']);
         $sql->bindParam(':username', $credentials['user']);
         $sql->execute();
 
